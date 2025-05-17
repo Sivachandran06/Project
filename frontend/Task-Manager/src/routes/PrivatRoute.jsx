ef38a96d54cf975ff1acba255b71aa72e8c1b0e4
sivachandran06 <sivachandran997@gamil.com>
2025-05-17 12:30:04 +0530

    setting file 3

--- frontend/Task-Manager/src/routes/PrivatRoute.jsx
+++ frontend/Task-Manager/src/routes/PrivatRoute.jsx
@@ -1,10 +1,7 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
-const PrivateRoute = ()=>{
-    return(
-        <>
-        <div>PrivateRoute</div>
-        </>
-    )
+const PrivateRoute = ({allowedRoles})=>{
+    return <Outlet/> 
 }
 export default PrivateRoute;
