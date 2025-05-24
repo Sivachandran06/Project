070c48c8b29a42b1a9f959bee73c2cc142759197
sivachandran06 <sivachandran997@gamil.com>
2025-05-24 11:13:43 +0530

    creating  loginUser

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -3,7 +3,7 @@ const bctypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // Gendrate JWT Token
-const generatrToke =(userId)=>{
+const generateToke =(userId)=>{
     return jwt.sign({id:userId}, process.env.JWT_SECRET, {expiresIn:"7d"});
 };
 
@@ -46,7 +46,7 @@ const registerUser = async (req, res)=>{
             email: user.email,
             profileImageUrl: user.profileImgUrl,
             role: user.role,
-            token:generatrToke(user._id),
+            token:generateToke(user._id),
         })
 
     }catch(error){
@@ -58,8 +58,29 @@ const registerUser = async (req, res)=>{
 // @router Post /api/auth/login
 // @access public
 const loginUser = async (req, res)=>{
-    try{
+    try{ 
+        const {email, password} = req.body
 
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({message:"Invalid email or password"})
+        }
+
+        //Compar Password
+        const isMatch = await bctypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(401).json({message:"Invalid email or Password"})
+        }
+
+        //Return User data with JWT
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            profileImageUrl: user.profileImageUrl,
+            toke: generateToke(user._id)
+        })
     }catch(error){
         res.status(500).json({message: "Server error", error: error.message})
     }
@@ -70,7 +91,11 @@ const loginUser = async (req, res)=>{
 // @acess private (Require JWT)
 const getUserProfile = async(req, res)=>{
     try{
-
+        const user = await User.findById(req.user.id).selected("-password");
+        if(!user){
+            return res.status(404).json({message: "user not forund"});
+        }
+        res.json(user);
     }catch(error){
         res.status(500).json({message: "Server error", error: error.message})
     }
