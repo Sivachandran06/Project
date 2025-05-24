ae373f2c020c63633040d2bad8efcb2863b2f309
sivachandran06 <sivachandran997@gamil.com>
2025-05-25 01:07:32 +0530

    updateUserProfile

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -106,6 +106,28 @@ const getUserProfile = async(req, res)=>{
 // @access Private (Require JWT)
 const updateUserProfile = async(req, res)=>{
     try{
+        const user = await User.findById(req.user.id);
+
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if(req.body.password){
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(req.body.password, salt);
+        }
+
+        const updateUser = await user.save();
+
+        res.json({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email,
+            role: updateUser.role,
+            token: generateToke(updateUser._id),
+        });
 
     }catch(error){
         res.status(500).json({message: "Server error", error: error.message})
---
+++ backend/middleware/uplodeMiddleware.js
@@ -0,0 +1,25 @@
+const multer = require("multer");
+
+//Configure store
+const storage = multer.diskStorage({
+    destination: (req, res, cb)=>{
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb)=>{
+        cb(null, `${Date.now()}-${file.originalname}`)
+    },
+});
+
+//file filter
+const fileFilter = (req, file, cb)=>{
+    const allowedType = ['image/jpeg', 'image/png', 'image/jpg'];
+    if(allowedType.includes(file.mimetype)){
+        cb(null,true)
+    }else{
+        cb(new Error('only .jpeg .png and .jpg formates are allowed'), false);
+    }
+};
+
+const upload = multer({storage, fileFilter});
+
+module.exports = upload;
