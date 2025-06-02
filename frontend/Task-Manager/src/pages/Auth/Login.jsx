423172dd5853f6e49e4324248703f8331ef53861
sivachandran06 <sivachandran997@gamil.com>
2025-06-02 23:54:39 +0530

    setting SignUp-page & Login-Page

--- frontend/Task-Manager/src/pages/Auth/Login.jsx
+++ frontend/Task-Manager/src/pages/Auth/Login.jsx
@@ -1,27 +1,71 @@
 import React, { useState } from "react";
 import AuthLayout from "../../componanets/Layouts/AuthLayouts";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import Input from "../../componanets/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
 
-const Login = ()=>{
-    const [email, setEmail]= useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
+const Login = () => {
+  const [email, setEmail]= useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-    const navigation = useNavigate();
+  const navigation = useNavigate();
 
-    // Handle Login from submit
-    const handelLogin = async(e)=>{
-        e.preventDefault();
+  // Handel login form Sumbmit
+  const handelLogin = async(e) => {
+    e.preventDefault();
+   
+    if(!validateEmail(email)){
+      setError("Please enter a valid email Address")
+      return;
+    }
+    if(!password){
+      setError("Please enter the Password");
+      return;
     }
 
-    return(
-        <AuthLayout>
-            <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-                <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
-                <p className="text-xs text-slate-700 mt-[5px] mb-6">please enter your details to log in</p>
-            </div>
-        </AuthLayout>
-        
-    )
+    setError("")
+  }
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">Please enter your details to log in</p>
+
+        <form onSubmit={handelLogin}>
+          <Input 
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john@gmail.com"
+            type="text"
+          />
+
+          <Input 
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+            type="password"
+          />
+
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            Login
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/signup">
+              SignUp
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  )
 }
+
 export default Login;
--- frontend/Task-Manager/src/pages/Auth/Signup.jsx
+++ frontend/Task-Manager/src/pages/Auth/Signup.jsx
@@ -1,9 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import AuthLayout from "../../componanets/Layouts/AuthLayouts";
+import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelector from "../../componanets/Inputs/ProfilePhotoSelector";
+import Input from "../../componanets/Inputs/Input";
 
 const Signup = ()=>{
+    const [profilePic, setProfilePic] = useState(null);
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [adminInviteToken, SetAdminInviteToken] = useState("");
+
+    const [error, setError] = useState(null)
+
+    //Handel signUp Form submit
+    const handeleSignup = async(e) => {
+        e.preventDefault();
+       
+        if(!fullName){
+          setError("Please enter full name")
+          return;
+        }
+        if(!validateEmail(email)){
+          setError("Please enter a valid email Address")
+          return;
+        }
+        if(!password){
+          setError("Please enter the Password");
+          return;
+        }
+    
+        setError("");
+        //SignUp Api Call
+      }
     return(
         <>
-        <div>Signup</div>
+        <AuthLayout>
+          <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+            <h3 className="text-xl font-semibold text-black"> Create an Account</h3>
+            <p className="text-xs text-slate-700 mt-[5px] mb-6">Join us today by entering your details below.</p>
+            
+            <ProfilePhotoSelector image = {profilePic} setImage = {setProfilePic} />
+          
+
+          <form onSubmit={handeleSignup}>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <Input
+                value={fullName}
+                onChange={({ target })=> setFullName(target.value)}
+                label="Full Name"
+                placeholder="John"
+                type="text"
+              />
+              
+              <Input 
+                value={email}
+                onChange={({ target }) => setEmail(target.value)}
+                label="Email Address"
+                placeholder="john@gmail.com"
+                type="text"
+              />
+
+              <Input 
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                placeholder="Min 8 Characters"
+                type="password"
+              />
+            </div>
+          </form>
+        </div>
+        </AuthLayout>
         </>
     )
 }
