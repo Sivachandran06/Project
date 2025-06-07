6165e13116109457c282da9e01f37e3671c951de
sivachandran06 <sivachandran997@gamil.com>
2025-06-08 00:37:24 +0530

    creating userContext

--- frontend/Task-Manager/src/App.jsx
+++ frontend/Task-Manager/src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter, Routes, Route, Outlet, Navigate } from "react-router-dom";
 import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
 import Dashboard from "./pages/Admin/Dashboard";
@@ -12,9 +12,11 @@ import MyTask from "./pages/User/MyTask";
 import ViewTaskDetails from "./pages/User/ViewTaskDetails";
 
 import PrivateRoute from "./routes/PrivatRoute";
+import UserProvider, { UserContext } from "./context/userContext";
 
 const App = () => {
   return (
+    <UserProvider>
     <div>
       <BrowserRouter>
         <Routes>
@@ -35,10 +37,25 @@ const App = () => {
             <Route path="/user/my-tasks" element={<MyTask />} />
             <Route path="/user/task-details/:id" element={<ViewTaskDetails />} />
           </Route>
+
+          {/* Default route */}
+          <Route path="/" element={<Root/>}/>
         </Routes>
       </BrowserRouter>
     </div>
+    </UserProvider>
   );
 };
 
 export default App;
+
+const Root =()=>{
+  const {user , loading}= useContext(UserContext);
+  if(loading) return<Outlet/>
+
+  if(!user){
+    return<Navigate to="login" />;
+  }
+
+  return user.role === "admin"? <Navigate to="/admin/dashboard"/>:<Navigate to="/user/dashboard"/>;
+};
