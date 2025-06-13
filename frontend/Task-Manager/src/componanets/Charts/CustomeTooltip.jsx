5c8efa49e176afcbc160123f0d60f6234ba2cc66
sivachandran06 <sivachandran997@gamil.com>
2025-06-13 23:49:16 +0530

    CustomeLegend-CustomToolTip-prepareChartData-addThousandsSeparator

---
+++ frontend/Task-Manager/src/componanets/Charts/CustomeTooltip.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CustomToolTip = ({active,payload})=>{
+    if(active && payload && payload.length){
+        return(
+            <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300">
+                <p className="text-xs font-semibold text-p">{payload[0].name}</p>
+                <p className="text-sm text-gray-600">
+                    Count:<span className="text-sm font-medium text-gray-900">{payload[0].value}</span>
+                </p>
+            </div>
+        )
+    }
+}
+export default CustomToolTip;
--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -6,6 +6,17 @@ import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPath";
 import { useNavigate } from "react-router-dom";
 import moment from 'moment';
+import { addThousandsSeparator } from "../../utils/helper";
+import InfoCard from "../../componanets/Cards/InfoCard";
+import { LuArrowRight } from "react-icons/lu";
+import TaskListTable from "../../componanets/Layouts/TaskListTable";
+import CustomPieChart from "../../componanets/Charts/CustomPieChart";
+import CustomBarChart from "../../componanets/Charts/CustomBarChart";
+
+const COLORS = ["#8D51FF", "#00B8DB", "#7BCE00"]; 
+
+
+
 const Dashboard = ()=>{
     useUserAuth();
 
@@ -15,7 +26,29 @@ const Dashboard = ()=>{
     
         const [dashboardData, setDashboardData] = useState(null);
         const [pieChartData, setPieChartdata]= useState([]);
-        const [barChartdata, setBarChartData]= useState([]);
+        const [barChartData, setBarChartData]= useState([]);
+
+        //Prepare Chart data
+        const prepareChartData = (data)=>{
+            const taskDistribution = data?.taskDistribution || null;
+            const taskPriorityLevels = data?.taskPriorityLevels || null;
+
+            const taskDistributionData = [
+                {status: "Pending", count: taskDistribution?.Pending || 0},
+                {status: "In Progress", count: taskDistribution?.InProgress || 0},
+                {status: "Completed", count: taskDistribution?.Completed || 0},
+            ];
+
+            setPieChartdata(taskDistributionData);
+
+            const priorityLevelData = [
+                {priority: "Low", count: taskPriorityLevels?.Low || 0},
+                {priority: "Medium", count: taskPriorityLevels?.Medium || 0},
+                {priority: "High", count: taskPriorityLevels?.High || 0},
+            ];
+
+            setBarChartData(priorityLevelData);
+        }
     
         const getDashboardData = async ()=>{
             try{
@@ -23,7 +56,7 @@ const Dashboard = ()=>{
                 console.log("Calling API:", API_PATHS.TASKS.GET_DASHBOARD_DATA);
                 if(response.data){
                     setDashboardData(response.data)
-                    console.log(response.data,"(response.data)")
+                    prepareChartData(response.data?.charts || null)
                 }
             }catch(error){
                 console.error("error fetching User:", error);
@@ -34,6 +67,11 @@ const Dashboard = ()=>{
     
             return ()=>{};
         },[]);
+
+        const onSeenMore = ()=>{
+            navigate('admin/tasks')
+        }
+
         return<DashboardLayout activeMenu={"Dashboard"}> 
             <div className="card my-5">
                 <div>
@@ -47,13 +85,73 @@ const Dashboard = ()=>{
 
                 <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5">
                     <InfoCard
-                       icon ={<IoMdCard/>}
                        label="Total Tasks"
                        value= {addThousandsSeparator(
                         dashboardData?.charts?.taskDistribution?.All || 0
                        )}
                        color ="bg-primary"
                        />
+                       <InfoCard
+                       label="Pending Tasks"
+                       value= {addThousandsSeparator(
+                        dashboardData?.charts?.taskDistribution?.Pending || 0
+                       )}
+                       color ="bg-violet-500"
+                       />
+                       <InfoCard
+                       label="In Progress Tasks"
+                       value= {addThousandsSeparator(
+                        dashboardData?.charts?.taskDistribution?.InProgress || 0
+                       )}
+                       color ="bg-cyan-500"
+                       />
+                       <InfoCard
+                       label="Completed Tasks"
+                       value= {addThousandsSeparator(
+                        dashboardData?.charts?.taskDistribution?.Completed || 0
+                       )}
+                       color ="bg-lime-500"
+                       />
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6">
+
+                <div>
+                    <div className="card">
+                        <div className="flex items-center justify-between">
+                            <h5 className="font-medium">Task Distribution</h5>
+                        </div>
+
+                        <CustomPieChart
+                            data={pieChartData}
+                            colors = {COLORS}
+                        />
+                    </div>
+                </div>
+
+                        <div>
+                    <div className="card">
+                        <div className="flex items-center justify-between">
+                            <h5 className="font-medium">Task Priority Levels</h5>
+                        </div>
+
+                        <CustomBarChart
+                            data={barChartData}
+                        />
+                    </div>
+                </div>
+
+                <div className="md:col-span-2">
+                    <div className="card">
+                        <div className="flex items-center justify-between">
+                            <h5 className="text-lg">Recent Tasks</h5>
+
+                            <button className="card-btn" onClick={onSeenMore}>
+                                See All<LuArrowRight className="text-base"/>
+                            </button>
+                        </div>
+                        <TaskListTable tabelData = {dashboardData?.recentTasks || []}/>
+                    </div>
                 </div>
             </div>
         </DashboardLayout>
