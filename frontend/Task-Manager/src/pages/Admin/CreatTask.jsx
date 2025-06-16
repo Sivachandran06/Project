cceeba4696730e4804f94e42ab5e68292cee5d9a
sivachandran06 <sivachandran997@gamil.com>
2025-06-16 23:51:26 +0530

    Edit_CreatTask

--- frontend/Task-Manager/src/pages/Admin/CreatTask.jsx
+++ frontend/Task-Manager/src/pages/Admin/CreatTask.jsx
@@ -1,9 +1,162 @@
-import React from "react";
+import React, { useState } from "react";
+import DashboardLayout from "../../componanets/Layouts/DashboardLayout";
+import { PRIORITY_DATA } from "../../utils/data";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPath";
+import toast from "react-hot-toast";
+import { useLocation, useNavigate } from "react-router-dom";
+import moment from "moment";
+import { LuTrash2 } from "react-icons/lu";
+import SelectDropdown from "../../componanets/Inputs/SelectDropdown";
+import SelectUsers from "../../componanets/Inputs/SelectUsers";
 
 const CreatTask = ()=>{
+
+    const location = useLocation();
+    const {taskId}= location.state || {};
+    const navigate = useNavigate();
+
+    const [taskData, setTaskData]= useState({
+        title: "",
+        description:"",
+        priority: "Low",
+        dueDate: null,
+        assignedTo:[],
+        todoChecklist:[],
+        attachments: [],
+    });
+
+    const [currentTask, setCurrentTask] = useState(null);
+
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const [openDeleteAlert, setOpenDeleteAlert] = useState(false);
+
+    const handelValueChange = (key, value)=>{
+        setTaskData((prevData)=>({...prevData, [key]:value}));
+    };
+
+    const clearData = ()=>{
+
+        //reset form
+        setTaskData({
+            title:"",
+            description:"",
+            priority:"Low",
+            dueDate: null,
+            assignedTo:[],
+            attachments:[],
+        })
+    };
+
+    //Create Task
+    const createTask = async()=>{};
+
+    //Update Task
+    const updateTask = async()=>{};
+
+    const handelSubmit = async()=>{};
+
+    //Get Task info by ID
+    const getTaskDetailsByID = async()=>{};
+
+    //Delete Task
+    const deleteTask = async()=>{};
     return(
         <>
-        <div>CreatTask</div>
+        <DashboardLayout activeMenu={"Create Task"}> 
+        <div className="mt-5">
+            <div className="grid grid-cols-1 md:grid-cols-4 mt-4">
+                <div className="form-card col-span-3">
+                    <div className="flex items-center justify-between">
+                        <h2 className="text-xl md:text-xl font-medium">
+                            {taskId ? "Update Task": "Create Task"}
+                        </h2>
+                        {taskId && (
+                            <button className="flex items-center gap-1.5 text-[13px] font-medium text-rose-500 bg-rose-50 px-1 border border-rose-100 hover:border-rose-300 cursor-pointer"
+                                    onClick={()=> setOpenDeleteAlert(true)}
+                            >
+                                <LuTrash2 className="text-base"/> Delete
+                            </button>
+                        )}
+                    </div>
+
+                    <div className="mt-4">
+                        <label className="text-xs font-medium text-slate-1000">
+                            Task title
+                        </label>
+
+                        <input 
+                            placeholder="Create App UI"
+                            className="form-input"
+                            value={taskData.title}
+                            onChange={({target})=>
+                            handelValueChange("title", target.value)
+                        }
+                        />
+                    </div>
+                    <div className="mt-3">
+                        <label className="text-xs font-medium text-slate-1000">
+                            Description
+                        </label>
+
+                        <textarea
+                            placeholder="Describe Task"
+                            className="form-input"
+                            rows={4}
+                            value={taskData.description}
+                            onChange={({target})=>
+                                handelValueChange("description", target.value)
+                            }
+                        />
+                    </div>
+                    <div className="grid grid-cols-12 gap-4 mt-2">
+                        <div className="col-span-6 md:col-span-4">
+                            <label className="text-xs font-medium text-slate-1000">
+                                Priority
+                            </label>
+
+                            <SelectDropdown
+                                options = {PRIORITY_DATA}
+                                value = {taskData.priority}
+                                onChange = {(value) =>handelValueChange("priority", value)}
+                                placeholder = "Select Priority"
+                            />
+                        </div>
+
+                        <div className="col-span-6 md:col-span-4">
+                            <label className="text-xs font-medium text-slate-1000">
+                                Due Date
+                            </label>
+
+                            <input
+                                placeholder="Create App UI"
+                                className="form-input"
+                                value={taskData.dueDate}
+                                onChange={({target})=> handelValueChange("dueDate",target.value)
+                            }
+                            type="date"
+                            />
+                        </div>
+                        <div className="col-span-12 md:col-span-3">
+                            <label className="text-xs font-medium text-slate-1000">
+                                Assign To
+                            </label>
+
+                            <SelectUsers
+                                selectedUsers = {taskData.assignedTo}
+                                setSelectedUsers = {(value)=>{
+                                handelValueChange("assignedTo", value);
+                                }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        </DashboardLayout>
         </>
     )
 }
