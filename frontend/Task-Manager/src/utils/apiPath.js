b40d6d87dbab2e47f809420f46bee52fa7ae72f9
sivachandran06 <sivachandran997@gamil.com>
2025-06-20 00:39:47 +0530

    Editing- API_PATH

--- frontend/Task-Manager/src/utils/apiPath.js
+++ frontend/Task-Manager/src/utils/apiPath.js
@@ -20,7 +20,7 @@ export const API_PATHS ={
         GET_DASHBOARD_DATA:"/api/tasks/dashboard-data",// Get dashboard Data
         GET_USER_DASHBOARD_DATA:"/api/tasks/user-dashboard-data", //Get User Dashboard Data
         GET_ALL_TASKS:"/api/tasks",//Get all Tasks(Admin: all, User:only assignment tasks)
-        GET_TASK_BY_ID: (taskId)=>`/api/tasks${taskId}`,//Get task by ID
+        GET_TASK_BY_ID: (taskId)=>`/api/tasks/${taskId}`,//Get task by ID
         CREATE_TASK: "/api/tasks", //Create a new task (Admin only)
         UPDATE_TASK: (taskId)=>`/api/tasks/${taskId}`, // Update task details
         DELETE_TASL: (taskId)=>`api/tasks/${taskId}`, //Delete a task (Admin only)
